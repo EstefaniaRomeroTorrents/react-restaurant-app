57c249dd47b57a9f1eb1e1faef5f731497cab211
EstefaniaRomero <estefaniaromero.torrents@gmail.com>
2023-02-21 02:53:03 +0100

    form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,21 +3,32 @@ import { Box } from "@mui/system";
 import "./Contact.css";
 import { useForm } from "react-hook-form";
 import * as React from "react";
-import moment from "moment";
+import { ErrorMessage } from "@hookform/error-message";
+
+import dayjs from "dayjs";
+import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
-import { DateTimePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 
 function Contact() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    criteriaMode: "all",
+  });
   const onSubmit = (data) => console.log(data);
   console.log(errors);
-  const [value, setValue] = React.useState(moment("2022-04-07"));
+  const [value, setValue] = React.useState(dayjs("2014-08-18T21:11:54"));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <Container maxWidth="lg">
@@ -35,57 +46,112 @@ function Contact() {
           </Box>
         </Grid>
         <Grid item={true} xs={12} sm={6} md={4}>
-          <Box
-            sx={{
-              display: "table-caption",
-              justifyContent: "center",
-            }}
-          >
-            <form onSubmit={handleSubmit(onSubmit)}>
+          <h1>Reservar Mesa</h1>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box
+              sx={{
+                width: 500,
+                maxWidth: "100%",
+              }}
+            >
               <input
+                fullWidth
                 type="text"
-                placeholder="Nombre completo"
-                {...register("Nombre completo", {
-                  required: true,
+                placeholder="First name"
+                {...register("Firstname", {
+                  required: "Se requiere un nombre.",
                   maxLength: 80,
+                  /*pattern: {
+                    value: /\d+/,
+                    message: "This input is number only.",
+                  },
+                  minLength: {
+                    value: 11,
+                    message: "This input must exceed 10 characters",
+                  },*/
                 })}
               />
+              {errors.Firstname && <p>{errors.Firstname.message}</p>}
+            </Box>
+            <Box>
               <input
                 type="text"
                 placeholder="Email"
                 {...register("Email", {
-                  required: true,
-                  pattern: /^\S+@\S+$/i,
+                  required: "Se requiere un Email.",
+                  pattern: {
+                    value: /^\S+@\S+$/i,
+                    message: "Se requiere un EMail correcto.",
+                  },
                 })}
               />
+              {errors.Email && <p>{errors.Email.message}</p>}
+            </Box>
+            <Box>
               <input
                 type="tel"
                 placeholder="Mobile number"
-                {...register("Mobile number", {
-                  required: true,
+                {...register("Mobilenumber", {
+                  required: "Se requiere un número de teléfono.",
                   minLength: 6,
                   maxLength: 12,
+                  pattern: {
+                    value: /\d+/,
+                    message: "Se requiere sólo números.",
+                  },
                 })}
               />
+              {errors.Mobilenumber && <p>{errors.Mobilenumber.message}</p>}
+            </Box>
+            <Box>
               <input
                 type="number"
-                placeholder="Personas"
-                {...register("personas", { min: 1, max: 99 })}
+                placeholder="Persons"
+                {...register("persons", {
+                  valueAsNumber: true,
+                  required: "Se requiere un número de personas.",
+                  pattern: {
+                    value: /\d+/,
+                    message: "Se requiere sólo números.",
+                  },
+                })}
               />
-              <LocalizationProvider dateAdapter={AdapterMoment}>
-                <DateTimePicker
-                  renderInput={(props) => <TextField {...props} />}
-                  label="DateTimePicker"
+              {errors.persons && <p>{errors.persons.message}</p>}
+            </Box>
+            <ErrorMessage
+              errors={errors}
+              name="multipleErrorInput"
+              render={({ messages }) => {
+                console.log("messages", messages);
+                return messages
+                  ? Object.entries(messages).map(([type, message]) => (
+                      <p key={type}>{message}</p>
+                    ))
+                  : null;
+              }}
+            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <Stack spacing={3}>
+                <DesktopDatePicker
+                  label="Date "
+                  inputFormat="MM/DD/YYYY"
                   value={value}
-                  onChange={(newValue) => {
-                    setValue(newValue);
-                  }}
+                  onChange={handleChange}
+                  renderInput={(params) => <TextField {...params} />}
                 />
-              </LocalizationProvider>
 
-              <input type="submit" />
-            </form>
-          </Box>
+                <TimePicker
+                  label="Time"
+                  value={value}
+                  onChange={handleChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </Stack>
+            </LocalizationProvider>
+
+            <input type="submit" />
+          </form>
         </Grid>
       </Grid>
     </Container>
